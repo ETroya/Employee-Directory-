63322031f1ed37bf39514ddcbeac3c70449e86ee
Edwin Troya <edtroyajr@gmail.com>
2021-03-31 00:23:46 -0700

    added the footer and wrapper

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./App.css";
-
 import Navbar from "./components/Navbar";
-
+import Wrapper from "./components/Wrapper";
+import Footer from "./components/Footer";
+import SearchForm from "./components/Searchform";
 
 function App() {
   return(
     <div>
       <Navbar />
+      <Wrapper>
+       <SearchForm />
+      </Wrapper>
+      <Footer />
+   
     </div>
   )
 };
--- src/components/Employeelist/index.js
+++ src/components/Employeelist/index.js
@@ -1,12 +1,21 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import API from "../../utils/API.JS"
 import Search from "../../components/Searchform"
 
 function Employeelist (props){
-    return(
-       <div>
-           <div className ="EmployeeList">
-               <img src ={props.src }
-       </div>
-    )
+   const[ results, setResults]=useState([]);
+   const[ sortedField, setSortedField ] = state(null);
+
+   async function getRandomUsers(){
+       try{
+           const response =await API.search();
+           setResults ( response.data.results )
+       }catch (error){
+           console.error(error); 
+       }
+   }
 }
+
+useEffect( ()=>{
+    getRandomUsers()
+}, [])
